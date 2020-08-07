1bed754c9634300a059670112f4e01b89861d6ac
SelinVural <slnvrlx@gmail.com>
2020-08-07 03:05:58 +0300

    İletişim sayfası, form, sıkça sorulan sorular sayfası oluşturuldu.

--- home/admin.py
+++ home/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 
 from home.models import *
 
+
+class ContactFormMessageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'subject', 'status']
+    list_filter = ['status']
+
+
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ['ordernumber', 'question', 'answer', 'status']
+    list_filter = ['status']
+
+
 admin.site.register(Setting)
+admin.site.register(FAQ, FAQAdmin)
+admin.site.register(ContactFormMessage, ContactFormMessageAdmin)
--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('aboutus', views.aboutus, name='aboutus'),
     path('references', views.references, name='references'),
+    path('contact', views.contact, name='contact'),
+    path('faq/', views.faq, name='faq')
 ]
